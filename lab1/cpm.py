c2f4955e2ba744d7db01b795f216d871180c5d43
RahimFatih <dfirat998@gmail.com>
2022-11-28 14:29:47 +0100

    pingwin

---
+++ lab1/cpm.py
@@ -0,0 +1,97 @@
+
+class Task:
+    def __init__(self,name,time):
+        self.name=name
+        self.time=time
+        self.ES=-1
+        self.EF=time
+        self.LS=-1
+        self.LF=-1
+        self.TF=0
+        self.critical_path=[]
+
+
+class CPMAlgorithm:
+    def __init__(self) -> None:
+        self.task_list=[]
+        self.task_connections=[]
+        self.max=0
+        pass
+    def import_data(self,path):
+        with open(path) as file:
+            mylist = file.read().splitlines()
+            N,M=mylist[0].split(" ")
+            tasks=mylist[1].split(" ")
+            
+            for count, task in enumerate(tasks):
+                self.task_list.append(Task(count,int(task)))
+            for connection in mylist[2].split("  "):
+                self.task_connections.append([int(x) - 1 for x in connection.split(" ")])
+            
+            self.task_connections=sorted(self.task_connections, key=lambda x: (x[0], x[1]))
+            
+
+    def calculate_ELFS(self):
+        change_flag=True
+        cycle_counter=0
+        while change_flag:
+            change_flag=False
+            for connection in self.task_connections:
+                if self.task_list[connection[1]].EF < self.task_list[connection[0]].EF + self.task_list[connection[1]].time:
+                    change_flag=True
+                    self.task_list[connection[1]].EF=self.task_list[connection[0]].EF+self.task_list[connection[1]].time 
+            cycle_counter=cycle_counter+1
+            if(cycle_counter==len(self.task_list)):
+                raise Exception("W grafie znajduje się cykl")
+        self.max=max(self.task_list,key=lambda x: x.EF).EF
+        for task in self.task_list:
+            task.ES = task.EF-task.time
+            task.LF = self.max
+        change_flag=True
+        while change_flag:
+            change_flag = False
+            for connection in self.task_connections:
+                if self.task_list[connection[0]].LF > self.task_list[connection[1]].LF - self.task_list[connection[1]].time:
+                    change_flag = True
+                    self.task_list[connection[0]].LF = self.task_list[connection[1]].LF - self.task_list[connection[1]].time
+        for task in self.task_list:
+            task.LS=task.LF - task.time
+            task.TF=task.LS-task.ES
+        critical_tasks = list(filter(lambda x: x.TF == 0,self.task_list))
+        critical_path=[]
+        critical_path.append(max(critical_tasks,key=lambda x: x.EF))
+        print("-----------------------------",critical_path[-1].name)
+        while True:
+            critical_task_EF=0
+            critical_task=0
+            for connection in list(filter(lambda x:x[1]==critical_path[-1].name,self.task_connections)):
+                if self.task_list[connection[0]].EF>critical_task_EF:
+                    critical_task=self.task_list[connection[0]]
+                    critical_task_EF=critical_task.EF
+            if critical_task!=0:
+                critical_path.append(critical_task)
+            if critical_path[-1].ES==0:
+                self.critical_path=critical_path
+                break
+        
+    def print_ELFS(self):
+        for task in self.task_list:
+            print("Name: "+str(task.name)+"   ES: ",task.ES, " EF: ", task.EF, " LS: ", task.LS, " LF: ", task.LF, " TF: ",task.TF  )
+
+        print("Max: ",self.max)
+        path="Critical path: start"
+    
+        for task in self.critical_path[::-1]:
+            path=path +"->"+ str(task.name)
+        print(path)
+
+
+
+
+
+if __name__ == "__main__":
+    alg=CPMAlgorithm()
+    alg.import_data("cpm_data.txt")
+    #alg.print_ELFS()
+    alg.calculate_ELFS()
+    alg.print_ELFS()
